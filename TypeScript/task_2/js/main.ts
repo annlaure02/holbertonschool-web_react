961a27c2fa17da8a48d05783043a69f714123216
annlaure02 <alaure.g02@gmail.com>
2023-08-03 14:15:41 +0200

    task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ interface DirectorInterface {
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -33,15 +33,26 @@ class Teacher implements TeacherInterface {
       return 'Cannot have a break';
   }
 
-  workDirectorTasks(): string {
+  workTeacherTasks(): string {
       return 'Getting to work';
   }
 }
 
-function createEmployee (salary: number | string): Director | Teacher {
+function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 } 
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+  return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if(isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
 
