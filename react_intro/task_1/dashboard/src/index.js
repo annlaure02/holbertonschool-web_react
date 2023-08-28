ac753a1266a9137ab931876fb1f165c3d80199f1
annlaure02 <alaure.g02@gmail.com>
2023-08-28 23:43:38 +0200

    task 1

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='root-notifications'>
-    <Notifications />
+      <Notifications />
     </div>
     <App />
   </React.StrictMode>
