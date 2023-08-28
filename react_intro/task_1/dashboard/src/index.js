18aecff8df6cdab4dfaa6e64a9ead20e26db9541
annlaure02 <alaure.g02@gmail.com>
2023-08-28 23:40:03 +0200

    task 1

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -7,7 +7,9 @@ import Notifications from './Notifications'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className='root-notifications'>
     <Notifications />
+    </div>
     <App />
   </React.StrictMode>
 );
