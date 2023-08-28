ab326536246221f0e47551018393433024c8e829
annlaure02 <alaure.g02@gmail.com>
2023-08-28 17:59:02 +0200

    task 3

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: ['./modules/header/header.js', './modules/body/body.js', './modules/footer/footer.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+  },
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ]
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    port: 8564,
+  },
+  mode: 'development',
+};
