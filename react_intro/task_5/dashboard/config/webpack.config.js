4bf85c43fae539cbf29ff97780ce77e00bff887d
annlaure02 <alaure.g02@gmail.com>
2023-08-29 22:09:52 +0200

    task 8

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -9,4 +9,26 @@ module.exports = {
   devServer: {
     hot: true,
   },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ]
+      }
+    ]
+  },
 }
