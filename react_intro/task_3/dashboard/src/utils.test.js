aaea42d4197e19df0213693e31f51fc578d4a1da
annlaure02 <alaure.g02@gmail.com>
2023-08-29 16:38:52 +0200

    task 4

--- react_intro/task_3/dashboard/src/utils.test.js
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -2,22 +2,23 @@ import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
 describe('function getFullYear', () => {
   test('Check if the function getFullYear return the actual year', () => {
-    expected(getFullYear()).toEqual(new Date().getFullYear());
-  })
+    expect(getFullYear()).toEqual(new Date().getFullYear());
+  });
 })
 
 describe('function getFooterCopy', () => {
   test('Check if the function getFooterCopy return Holberton School if true', () => {
-    expected(getFooterCopy(true)).toEqual('Holberton School');
-  })
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
 
   test('Check if the function getFooterCopy return Holberton School main dashboard if false', () => {
-    expected(getFooterCopy(false)).toEqual('Holberton School main dashboard');
-  })
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+  });
 })
 
 describe('function getLatestNotification', () => {
-  test('check if the function getLatestNotification return the strinf', () => {
-    expected(getLatestNotification().toEqual('<strong>Urgent requirement</strong> - complete by EOD'));
-  })
+  test("check if the function getLatestNotification return the string", () => {
+		expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
+	});
 })
+
