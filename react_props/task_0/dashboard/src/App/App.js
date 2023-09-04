48e0a65b657809981681f1a74a67c4370b4e8f76
annlaure02 <alaure.g02@gmail.com>
2023-09-04 13:34:43 +0200

    task 0

---
+++ react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Notifications />
+      </div>
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
