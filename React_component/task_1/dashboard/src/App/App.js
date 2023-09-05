0a88507cf85a776098578de40f70454c9789751d
annlaure02 <alaure.g02@gmail.com>
2023-09-05 22:38:14 +0200

    task 1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -11,6 +11,22 @@ import { getLatestNotification } from '../utils/utils';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress)
+  }
+  
+  handleKeyPress(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
   }
 
   render() {
