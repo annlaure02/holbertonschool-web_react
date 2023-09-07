f7bc1aa86a65b82597e23009ffffa167504e0631
annlaure02 <alaure.g02@gmail.com>
2023-09-07 15:03:26 +0200

    task 10

--- React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -15,6 +15,13 @@ class Notifications extends Component {
     console.log(`Notification ${id} has been marked as read`);
   };
 
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.listNotifications.length > this.props.listNotifications.length) {
+      return true;
+    }
+    return false;
+  }  
+
   render() {
     const { displayDrawer, listNotifications } = this.props;
 
