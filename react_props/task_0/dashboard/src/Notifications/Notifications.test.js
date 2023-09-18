962f9de1dcae77731b9ee3203674b1c27e79ac97
annlaure02 <alaure.g02@gmail.com>
2023-09-18 17:07:51 +0200

    fixed tests

--- react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Notifications from './Notifications';
 import { shallow } from 'enzyme';
 
@@ -8,11 +9,11 @@ describe('test Notifications', () => {
 
   it('Notifications renders three list items', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find("li")).to.have.lengthOf(3);
+    expect(wrapper.find("li")).toHaveLength(3);
   })
 
   it('Notifications renders the text Here is the list of notifications', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.contains(<p>Here is the list of notifications</p>)).to.equal(true);
+    expect(wrapper.contains(<p>Here is the list of notifications</p>)).toEqual(true);
   })
 });
