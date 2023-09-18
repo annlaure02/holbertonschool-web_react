6481e2280ce8a8b6ad1584bee3617a9285f580a7
annlaure02 <alaure.g02@gmail.com>
2023-09-18 18:12:46 +0200

    task 2

--- react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Notifications from './Notifications';
 import NotificationItem from './NotificationItem';
 import { shallow } from 'enzyme';
@@ -9,16 +10,16 @@ describe('test Notifications', () => {
 
   it('Notifications renders NotificationItem elements', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find(<NotificationItem />)).to.have.lengthOf(3);
+    expect(wrapper.find(NotificationItem)).toHaveLength(3);
   });
 
   it('Notifications renders NotificationItem elements', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find(<NotificationItem />).first().html()).to.equal('<li data-priority="default">New course available</li>');
+    expect(wrapper.find(NotificationItem).first().html()).toEqual('<li data-priority="default">New course available</li>');
   });
 
   it('Notifications renders the text Here is the list of notifications', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.contains(<p>Here is the list of notifications</p>)).to.equal(true);
-  });
+    expect(wrapper.contains(<p>Here is the list of notifications</p>)).toEqual(true);
+  })
 });
